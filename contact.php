6de962aec97e61387257ec9701676db3f977d08c
Noa.O <noa.obringer@gmail.com>
2025-03-19 14:03:38 +0100

    fix: homepage integration

--- contact.php
+++ contact.php
@@ -10,7 +10,14 @@
 </head>
 <body>
 
+<?php
+session_start();
 
+if (!isset($_SESSION['csrf_token']) || empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+
+?>
 
 <?php include 'templates/header.php'; ?>
 
